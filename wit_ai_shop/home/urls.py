c76fa2bd5f5806fd89b07877dafcfe7887d50db2
Prachi Aggarwal <prachiaggarwal476@gmail.com>
2020-09-05 04:25:09 +0530

    Products update

--- wit_ai_shop/home/urls.py
+++ wit_ai_shop/home/urls.py
@@ -3,5 +3,4 @@ from home import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mic',views.mic,name='mic'),
-    # path(str(views.section_id())+'^', views.home, name='#deal-href')
 ]
--- wit_ai_shop/home/views.py
+++ wit_ai_shop/home/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from wit_files import wit_speech
-
+import json
 
 # Create your views here.
 
 def home(request):
     if request.method == 'GET':
-        return render(request, 'index.html')
+        return render(request, 'index.html')       
 
 def mic(request):
     text = wit_speech.RecognizeSpeech('myspeech.wav', 4)
     print(text)
-    return redirect(reverse('home')+ '#deal-href')
+    if request.method == 'POST':
+        data = json.dumps({'id':'#deal-href'})
+        return render(request, 'index.html', {'data':data})
+
