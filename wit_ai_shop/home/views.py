c3252ef04d4564b16a6fbc26eff20252555d5ee0
gautamgupta1811 <gautamgupta1811@gmail.com>
2020-09-04 18:03:31 +0530

    aa

--- wit_ai_shop/home/views.py
+++ wit_ai_shop/home/views.py
@@ -7,12 +7,10 @@ from wit_files import wit_speech
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
 
 def mic(request):
     text = wit_speech.RecognizeSpeech('myspeech.wav', 4)
     print(text)
     return redirect(reverse('home')+ '#deal-href')
-
-def section_id():
-    return '#deal-href'
