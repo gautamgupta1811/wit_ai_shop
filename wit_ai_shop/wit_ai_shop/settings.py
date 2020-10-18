0b7e66aa432a36b49e1afc677274f42b0ff7156a
Gautam Gupta <gautamgupta1811@gmail.com>
2020-10-18 11:15:27 +0530

    Update settings.py

--- wit_ai_shop/wit_ai_shop/settings.py
+++ wit_ai_shop/wit_ai_shop/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pgd1ana@ccd1)*&rx)m1-q$*1msiu@+6q0&h959aibd*404ung'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
